9af366d1e77193efdad81d423d228a94958f5039
Nick Touchette <touchette@gmail.com>
2021-02-02 18:13:19 -0500

    add webpack server and hot reload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'sample'),
+        port: 9000,
+        host: 'localhost',
+    }
 });
