e1ac4d7fc66aac08a196228fba1e09282a29021d
Nick Touchette <touchette@gmail.com>
2021-02-15 09:40:56 -0500

    hide registration marks for inerts

--- src/entities/Block.js
+++ src/entities/Block.js
@@ -27,13 +27,17 @@ const Block = (entity) => {
     group.position.y = entity.props.position.y;
     group.position.z = entity.props.position.z;
   }
+
+  const registrationMark = new TransformNode('Registraton Mark');
+  registrationMark.parent = group;
+  registrationMark.setEnabled(false);
   
   for (let i = 0; i < block.entities.length; i++) {
     const {type, ...props} = block.entities[i];
 
     const {mesh} = new Entity(type, props, entity.source);
 
-    if (mesh) mesh.parent = group;
+    if (mesh) mesh.parent = registrationMark;
   }
 
   return group;
